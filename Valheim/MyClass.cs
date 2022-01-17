8daea807a877abfec12b130ca20c158541def09e
Banderi <banderi002@gmail.com>
2022-01-17 13:11:03 +0100

    Cleaned up code, fixed object re-assignment when loading worlds

--- Valheim/MyClass.cs
+++ Valheim/MyClass.cs
@@ -35,19 +35,26 @@ namespace ValheimMod
 		
 		//
 		
-		static private bool initialized = false;
 		static GameObject newPinsParent;
 		static Quaternion lastPlayerRot;
 		static Vector2 origin;
 		static Vector2 playerMinimapPinPos;
 		
-		static void reparentPins(GameObject map) {
-			foreach (Transform child in map.transform) {
-				child.gameObject.transform.SetParent(newPinsParent.transform);
+		static void reparentPins(GameObject A, GameObject B) {
+			if (A == null) {
+				logger.LogError("Can not reparent: A is NULL");
+				return;
+			}
+			if (B == null) {
+				logger.LogError("Can not reparent: B is NULL");
+				return;
+			}
+			foreach (Transform child in A.transform) {
+				child.gameObject.transform.SetParent(B.transform);
 			}
 		}
 		static void init(GameObject map) {
-			if (initialized)
+			if (newPinsParent != null)
 				return;
 			else {
 				logger.LogWarning("MOD: Initializing...");
@@ -57,35 +64,55 @@ namespace ValheimMod
 				newPinsParent.transform.position = map.transform.position;
 				newPinsParent.transform.SetParent(map.transform.parent);
 				
-//				reparentPins(map);
-				
-				initialized = true;
 				logger.LogWarning("MOD: ...Done!");
 			}
 		}
 		
-		[HarmonyPatch(typeof(Minimap), "ClearPins")]
-		class PinsUpdate2 { static bool Prefix() { logger.LogWarning("Clearing pins!"); return true; }}
 		[HarmonyPatch(typeof(Minimap), "ForceRegen")]
 		class PinsUpdate3 { static bool Prefix() { logger.LogWarning("Forcing regen!"); return true; }}
 		
 		//
-		[HarmonyPatch(typeof(Minimap), "AddPin")]
-		class PinsUpdate1
-		{
-			static void Postfix(ref GameObject ___m_mapSmall, Vector3 pos, Minimap.PinType type, string name)
-			{
-				init(___m_mapSmall);
-				reparentPins(___m_mapSmall);
-				logger.LogWarning(String.Format("Adding pin! {0} {1} {2}", pos, name, type));
+		[HarmonyPatch(typeof(Minimap), "ClearPins")]
+		class MinimapClearPins {
+			static void Prefix(ref GameObject ___m_mapSmall) {
+				logger.LogWarning("Clearing pins!");
 			}
 		}
-		[HarmonyPatch(typeof(Minimap), "UpdatePins")]
-		class MinimapRotation
-		{
-			static void Postfix(ref GameObject ___m_mapSmall, ref GameObject ___m_mapLarge, ref UnityEngine.RectTransform ___m_smallMarker)
-			{
+		[HarmonyPatch(typeof(Minimap), "RemovePin")]
+		[HarmonyPatch(new Type[] { typeof(Minimap.PinData) })]
+		class MinimapRemovePin {
+			static void Prefix(ref GameObject ___m_mapSmall, Minimap.PinData pin) {
+				logger.LogWarning(String.Format("Removing pin: {0} {1} {2}", pin.m_pos, pin.m_name, pin.m_type));
+			}
+		}
+		[HarmonyPatch(typeof(Minimap), "AddPin")]
+		class MinimapAddPin {
+			static void Postfix(ref GameObject ___m_mapSmall, Vector3 pos, Minimap.PinType type, string name) {
+				logger.LogWarning(String.Format("Adding pin: {0} {1} {2}", pos, name, type));
+				init(___m_mapSmall);
+				reparentPins(___m_mapSmall, newPinsParent);
+			}
+		}
+		[HarmonyPatch(typeof(Minimap), "Update")]
+		class MinimapRotation {
+			static void Postfix(ref GameObject ___m_mapSmall, ref GameObject ___m_mapLarge,
+			                    ref UnityEngine.RectTransform ___m_smallMarker,
+			                    ref UnityEngine.RectTransform ___m_smallShipMarker,
+			                    ref RectTransform ___m_windMarker) {
+				// rotate the minimap
 				___m_mapSmall.transform.rotation = Quaternion.Euler(0, 0, lastPlayerRot.eulerAngles.y);
+				
+				// rotate the player marker
+				___m_smallMarker.transform.rotation = Quaternion.Euler(0, 0, 0);
+				
+				// rotate the ship marker
+				Quaternion shipRotation = ___m_smallShipMarker.rotation;
+				___m_smallShipMarker.rotation = Quaternion.Euler(0, 0, lastPlayerRot.eulerAngles.y) * shipRotation;
+				
+				// rotate the wind marker
+				Quaternion windRotation = ___m_windMarker.rotation;
+				___m_windMarker.rotation = Quaternion.Euler(0, 0, lastPlayerRot.eulerAngles.y) * windRotation;
+				
 				return;
 				logger.LogWarning(String.Format("A: {0} {1} {2} {3} {4}",
 				                                ___m_mapSmall.transform.position,
@@ -97,10 +124,8 @@ namespace ValheimMod
 			}
 		}
 		[HarmonyPatch(typeof(Minimap), "WorldToMapPoint")]
-		class MinimapCenterPos
-		{
-			static private void Postfix(Vector3 p, float mx, float my)
-			{
+		class MinimapCenterPos {
+			static private void Postfix(Vector3 p, float mx, float my) {
 				Player localPlayer = Player.m_localPlayer;
 				Vector3 position = localPlayer.transform.position;
 				if (p == position) { // this is the local player pin's RELATIVE (UV) position used to calculating the anchor points later
@@ -110,19 +135,14 @@ namespace ValheimMod
 			}
 		}
 		[HarmonyPatch(typeof(Minimap), "UpdatePlayerMarker")]
-		class MinimapCenterRot
-		{
-			static private void Postfix(Quaternion playerRot, ref UnityEngine.RectTransform ___m_smallMarker)
-			{
+		class MinimapCenterRot {
+			static private void Postfix(Quaternion playerRot, ref UnityEngine.RectTransform ___m_smallMarker, ref UnityEngine.RectTransform ___m_smallShipMarker) {
 				lastPlayerRot = playerRot;
-				___m_smallMarker.transform.rotation = Quaternion.Euler(0, 0, 0);
 			}
 		}
 		[HarmonyPatch(typeof(Minimap), "MapPointToLocalGuiPos")]
-		class MinimapIcons
-		{
-			static private void Prefix(Minimap.MapMode ___m_mode, ref float mx, ref float my, UnityEngine.UI.RawImage img)
-			{
+		class MinimapIcons {
+			static private void Prefix(Minimap.MapMode ___m_mode, ref float mx, ref float my, UnityEngine.UI.RawImage img) {
 				if (___m_mode != Minimap.MapMode.Large) {
 					
 					Vector2 A = new Vector2(mx, my);
@@ -133,15 +153,6 @@ namespace ValheimMod
 					mx = B.x;
 					my = B.y;
 				}
-			}
-		}
-		[HarmonyPatch(typeof(Minimap), "UpdateWindMarker")]
-		class MinimapWind
-		{
-			static private void Postfix(ref RectTransform ___m_windMarker)
-			{
-				Quaternion windRotation = ___m_windMarker.rotation;
-				___m_windMarker.rotation = Quaternion.Euler(0, 0, lastPlayerRot.eulerAngles.y) * windRotation;
 			}
 		}
 	}
